77c8b173e64c41b03ec719480be8d255b1cd49a0
Essahlaoui <essahlaouifouad@gmail.com>
2021-01-06 01:11:29 +0100

    Update RDP.sh

--- RDP.sh
+++ RDP.sh
@@ -3,7 +3,7 @@ printf "Installing RDP Be Patience... " >&2
 {
 sudo useradd -m FOUAD
 sudo adduser FOUAD sudo
-echo 'FOUAD:2020' | sudo chpasswd
+echo 'FOUAD:2003' | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 sudo apt-get update
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
